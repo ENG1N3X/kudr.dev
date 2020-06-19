b63b853e342338870627d975f34f45f00569fc1e
eng1n3x <eng1n3x.developer@gmail.com>
2020-06-19 19:44:05 +0300

    update layout markup

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,26 @@
 <template>
-  <nuxt />
+  <div class="default-layout">
+    <app-header />
+    <nuxt />
+    <app-footer />
+  </div>
 </template>
+
+<script>
+import AppHeader from '~/components/header/header.vue'
+import AppFooter from '~/components/footer/footer.vue'
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.default-layout
+  min-height: 100vh
+  display: flex
+  flex-direction: column
+</style>
