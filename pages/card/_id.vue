d9011cbac224e8bc15ca2f63de59ce9b74336d25
eng1n3x <eng1n3x.developer@gmail.com>
2020-06-19 19:44:23 +0300

    add card page

---
+++ pages/card/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <p class="color-000">{{ card._id }}</p>
+        <p class="color-000">{{ card.title }}</p>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, $axios }) {
+    try {
+      const card = await $axios.get(`/api/v1/card/getById/${params.id}`)
+      return { card: card.data }
+    } catch (e) {
+      console.error('Card not found', e)
+    }
+  }
+}
+</script>
