597d58582ccebcc842a8074191cdb12c7954fd29
eng1n3x <eng1n3x.developer@gmail.com>
2020-07-10 17:35:27 +0300

    users page components

---
+++ components/users/ListItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-row>
+    <b-col>
+      <p class="usersInfo">{{ user.name }}</p>
+    </b-col>
+    <b-col>
+      <p class="usersInfo">{{ user.login }}</p>
+    </b-col>
+    <b-col>
+      <p class="usersInfo">{{ user.created }}</p>
+    </b-col>
+    <b-col>
+      <p class="usersInfo admin" v-if="user.admin == 1">Администратор</p>
+      <p class="usersInfo owner" v-else-if="user.admin == 2">Основатель</p>
+      <p class="usersInfo default" v-else>Пользователь</p>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
