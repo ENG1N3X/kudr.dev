20a0bac82ff431b78a12da576fbc9ea46972d513
eng1n3x <eng1n3x.developer@gmail.com>
2020-07-10 17:37:13 +0300

    new users page

---
+++ pages/users.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <app-users-list />
+  </div>
+</template>
+
+<script>
+import AppUsersList from '~/components/users/List.vue'
+
+export default {
+  head: {
+    title: 'Пользователи'
+  },
+  layout: 'default',
+  middleware: 'auth',
+  components: {
+    AppUsersList
+  },
+  async fetch({ store }) {
+    try {
+      if (store.getters['users/usersList/usersList'].length === 0) {
+        await store.dispatch('users/usersList/getAllUsers')
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+</script>
