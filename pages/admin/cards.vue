0e21bcbf025cb3b461e4f8084cd18bca16f8d53f
eng1n3x <eng1n3x.developer@gmail.com>
2020-07-10 17:36:55 +0300

    add admin page

---
+++ pages/admin/cards.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p class="color-000">123</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Админ-панель'
+  },
+  layout: 'admin'
+}
+</script>
---
+++ pages/admin/users.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p class="color-000">1234</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Админ-панель'
+  },
+  layout: 'admin'
+}
+</script>
