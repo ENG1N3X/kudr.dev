57e1bb3afa1b7f6856fa68a229ffec60d5e6e1e7
eng1n3x <eng1n3x.developer@gmail.com>
2020-07-10 17:35:44 +0300

    admin page components

---
+++ components/admin/Nav.vue
@@ -0,0 +1,7 @@
+<template>
+  <b-nav vertical class="adminNav">
+    <h4 class="mainTitle mb-2">Меню</h4>
+    <nuxt-link to="/admin/cards" class="adminNav-link">Карточки</nuxt-link>
+    <nuxt-link to="/admin/users" class="adminNav-link">Пользователи</nuxt-link>
+  </b-nav>
+</template>
