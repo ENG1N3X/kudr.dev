89e7651a0afdd0d01361053dc44a3b383d58a076
eng1n3x <eng1n3x.developer@gmail.com>
2020-07-17 19:21:32 +0300

    Добавлен адаптив

--- pages/card/_id.vue
+++ pages/card/_id.vue
@@ -1,17 +1,17 @@
 <template>
   <b-container class="item">
-    <b-row>
-      <b-col cols="5" v-if="card.image">
+    <b-row class="justify-content-center align-items-center">
+      <b-col cols="12" lg="5" v-if="card.image" class="mb-20">
         <div class="itemImage">
-          <img :src="`../cards/nuxt-card.png`" :alt="card.title" />
+          <img :src="`../cards/nuxt-card.png`" :alt="card.title" class="img-fluid" />
         </div>
       </b-col>
-      <b-col class="bg-main" style="border-radius: 12px; padding: 20px;">
+      <b-col cols="12" lg="7" class="bg-main mb-20" style="border-radius: 12px; padding: 20px;">
         <h1 class="itemTitle">{{ card.title }}</h1>
         <small class="itemSubtitle">{{ card.subtitle }}</small>
         <p class="itemDesc">{{ card.description }}</p>
-        <span class="itemCreated">Добавлено {{ card.created }}</span>
-        <span class="itemModified" v-if="card.modified">Изменено {{ card.modified }}</span>
+        <time class="itemCreated">Добавлено {{ card.created }}</time>
+        <time class="itemModified" v-if="card.modified">Изменено {{ card.modified }}</time>
         <a :href="`${card.link}`" class="btn btn-light" v-if="card.link">Ссылка</a>
       </b-col>
     </b-row>
