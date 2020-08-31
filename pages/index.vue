c51ba8345c42c549fbe65df35114267f2a1fca2c
eng1n3x <eng1n3x.developer@gmail.com>
2020-08-31 21:51:08 +0300

    fix mobile v3

--- pages/index.vue
+++ pages/index.vue
@@ -18,34 +18,17 @@ export default {
     AppAuthor,
     AppCardsList
   },
-  mounted() {
-    this.getData()
-  },
-  methods: {
-    async getData() {
-      try {
-        if (!this.$store.getters['cards/cardsList'].length) {
-          await this.$store.dispatch('cards/getAllCards')
-        }
-        if (!this.$store.getters['tunings/tuningsList'].length) {
-          await this.$store.dispatch('tunings/getAllTunings')
-        }
-      } catch (e) {
-        console.error(e)
+  async fetch({ store }) {
+    try {
+      if (!store.getters['cards/cardsList'].length) {
+        await store.dispatch('cards/getAllCards')
       }
+      if (!store.getters['tunings/tuningsList'].length) {
+        await store.dispatch('tunings/getAllTunings')
+      }
+    } catch (e) {
+      console.error(e)
     }
   }
-  // async fetch({ store }) {
-  //   try {
-  //     if (!store.getters['cards/cardsList'].length) {
-  //       await store.dispatch('cards/getAllCards')
-  //     }
-  //     if (!store.getters['tunings/tuningsList'].length) {
-  //       await store.dispatch('tunings/getAllTunings')
-  //     }
-  //   } catch (e) {
-  //     console.error(e)
-  //   }
-  // }
 }
 </script>
